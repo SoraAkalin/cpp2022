c0ceb8282a702c4d0bfd8bb4370dce4080ae3d63
SoraAkalin-02 <SoraAkalin@outlook.com>
2023-02-13 17:37:50 +0800

    苏菲

--- system.cpp
+++ system.cpp
@@ -35,39 +35,6 @@ void MenuSelect()
     cout << "\n";
 }
 
-// static void inputPassword(string &str, int size)
-// {
-//     char c;
-//     int count = 0;
-//     char *password = new char[size]; // 动态申请空间
-//     while ((c = getch()) != 'r')
-//     {
-//         // if (c == 8) { // 退格
-//         // 	if (count == 0) {
-//         // 		continue;
-//         // 	}
-//         // 	putchar('b'); // 回退一格
-//         // 	putchar(' '); // 输出一个空格将原来的*隐藏
-//         // 	putchar('b'); // 再回退一格等待输入
-//         // 	count--;
-//         // }
-//         if (count == size - 1)
-//         { // 最大长度为size-1
-//             continue;
-//         }
-//         if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'))
-//         {                 // 密码只可包含数字和字母
-//             putchar('*'); // 接收到一个字符后, 打印一个*
-//             password[count] = c;
-//             count++;
-//         }
-//     }
-//     password[count] = ' ';
-//     str = password;
-//     delete[] password; // 释放空间
-//     cout << endl;
-// }
-
 // 登录界面
 void LoginIn()
 {
