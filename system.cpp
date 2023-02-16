b4cc6c26548ae2191d6518a9279c1f00014c7592
SoraAkalin <SoraAkalin@outlook.com>
2023-02-17 00:18:01 +0800

    全部功能完成ver1.0

--- system.cpp
+++ system.cpp
@@ -91,6 +91,24 @@ struct Patient
     string doctorname;
 };
 
+// 接种疫苗信息
+struct VacInfo
+{
+    string name;
+    string sex;
+    int age;
+    string date;
+    string phone;
+    string vac;
+};
+
+// 疫苗信息数组
+struct VacInfoList
+{
+    struct VacInfo vacinfo[MAX];
+    int V_size;
+};
+
 // 患者数组
 struct PatientList
 {
@@ -144,7 +162,14 @@ bool JudgePhone(string strWord)
 // 判断邮箱是否正确
 bool JudgeEmail(string strWord)
 {
-    bool res = regex_match(strWord, regex("^[A-Za-z0-9]+@[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(?:\.com)+$"));
+    bool res = regex_match(strWord, regex("^[A-Za-z0-9]+@[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(?:\\.com)+$"));
+    return res;
+}
+
+// 判断日期格式是否正确
+bool JudgeDate(string strWord)
+{
+    bool res = regex_match(strWord, regex("^(?:0?[1-9]|[1-2]\\d|30|31)\\/(?:1[0-2]|0?[1-9])\\/\\d{2}$"));
     return res;
 }
 
@@ -265,7 +290,7 @@ void AddPatient(bool sign)
     system("pause");
     system("cls");
 }
-// what
+
 // 排序输出信息
 void SortPatient()
 {
@@ -339,40 +364,193 @@ void SortPatient()
 }
 
 // 查询患者信息
-void SearchPatient(PatientList *patientlist)
+void SearchPatient()
 {
     char retry;
     do
     {
         system("cls");
+        PatientList patientlist;
+        patientlist.patient[MAX];
+        patientlist.P_size = 0;
+        ifstream ifs;
+        ifs.open("Record1.txt", ios::in);
+        if (!ifs.is_open())
+        {
+            cout << "文件打开失败" << endl;
+            return;
+        }
+        string buf_s;
+        // int buf_i;
+        bool flag = false;
+        // int count = 0;
+        do
+        {
+            getline(ifs, buf_s);
+            patientlist.patient[patientlist.P_size].name = buf_s;
+            getline(ifs, buf_s);
+            patientlist.patient[patientlist.P_size].sex = buf_s;
+            getline(ifs, buf_s);
+            patientlist.patient[patientlist.P_size].age = atoi(buf_s.c_str());
+            getline(ifs, buf_s);
+            patientlist.patient[patientlist.P_size].address = buf_s;
+            getline(ifs, buf_s);
+            patientlist.patient[patientlist.P_size].phone = buf_s;
+            getline(ifs, buf_s);
+            patientlist.patient[patientlist.P_size].email = buf_s;
+            getline(ifs, buf_s);
+            patientlist.patient[patientlist.P_size].symptom = buf_s;
+            getline(ifs, buf_s);
+            patientlist.patient[patientlist.P_size].doctorname = buf_s;
+            patientlist.P_size++;
+        } while (getline(ifs, buf_s));
+        ifs.close();
+
         TopInfo();
         cout << "\t！！！！！！！患者信息查询！！！！！！！" << endl;
         string pname;
-        bool flag = 0;
+        bool flag1 = 0;
         cout << "\t\t输入患者姓名：";
         cin >> pname;
-        for (int i = 0; i < patientlist->P_size; i++)
+        for (int i = 0; i < patientlist.P_size; i++)
         {
-            if (pname == patientlist->patient[i].name)
+            if (pname == patientlist.patient[i].name)
             {
-                flag = 1;
+                flag1 = 1;
                 cout << "姓名\t性别\t年龄\t住址\t\t手机\t\t邮箱\t\t\t症状\t主治医生" << endl;
                 cout << "============================================================================================" << endl;
-                cout << patientlist->patient[i].name << "\t" << patientlist->patient[i].sex << "\t"
-                     << patientlist->patient[i].age << "\t" << patientlist->patient[i].address << "\t"
-                     << patientlist->patient[i].phone << "\t" << patientlist->patient[i].email << "\t"
-                     << patientlist->patient[i].symptom << "\t" << patientlist->patient[i].doctorname << endl;
+                cout << patientlist.patient[i].name << "\t" << patientlist.patient[i].sex << "\t"
+                     << patientlist.patient[i].age << "\t" << patientlist.patient[i].address << "\t"
+                     << patientlist.patient[i].phone << "\t" << patientlist.patient[i].email << "\t"
+                     << patientlist.patient[i].symptom << "\t" << patientlist.patient[i].doctorname << endl;
             }
         }
-        if (flag == 0)
+        if (flag1 == 0)
         {
             cout << "\t\t未查询到该患者信息！" << endl;
         }
         cout << "\t\t是否继续查询[Y/N]：";
         cin >> retry;
     } while (retry == 'Y' || retry == 'y');
+    cout << "\t\t感谢使用！";
+    system("pause");
+    system("cls");
 }
 
+// 添加疫苗信息
+void AddVacInfo()
+{
+    VacInfoList vacinfolist;
+    vacinfolist.vacinfo[MAX];
+    vacinfolist.V_size = 0;
+    // char flag;
+    // do
+    // {
+    system("cls");
+    TopInfo();
+    int i = vacinfolist.V_size;
+    cout << "\t！！！！！！！疫苗接种系统！！！！！！！" << endl;
+    do
+    {
+        cout << "\t\t接种者姓名：";
+        cin >> vacinfolist.vacinfo[i].name;
+        if (GetStringWords(vacinfolist.vacinfo[i].name) < 2 || GetStringWords(vacinfolist.vacinfo[i].name) > 6)
+        {
+            cout << "\t输入无效，姓名只能由2-6个字符组成。请重新输入！" << endl;
+        }
+    } while (GetStringWords(vacinfolist.vacinfo[i].name) < 2 || GetStringWords(vacinfolist.vacinfo[i].name) > 6);
+    do
+    {
+        cout << "\t\t性别[男/女]：";
+        cin >> vacinfolist.vacinfo[i].sex;
+        if (vacinfolist.vacinfo[i].sex != "男" && vacinfolist.vacinfo[i].sex != "女")
+        {
+            cout << "\t输入无效，性别只能为男或女。请重新输入！" << endl;
+        }
+    } while (vacinfolist.vacinfo[i].sex != "男" && vacinfolist.vacinfo[i].sex != "女");
+    do
+    {
+        cout << "\t\t年龄：";
+        cin >> vacinfolist.vacinfo[i].age;
+        if (vacinfolist.vacinfo[i].age < 1 || vacinfolist.vacinfo[i].age > 120)
+        {
+            cout << "\t输入无效，年龄只能为1-120岁。请重新输入！" << endl;
+        }
+    } while (vacinfolist.vacinfo[i].age < 1 || vacinfolist.vacinfo[i].age > 120);
+    do
+    {
+        cout << "\t\t接种日期（dd/mm/yy）：";
+        cin >> vacinfolist.vacinfo[i].date;
+        if (!JudgeDate(vacinfolist.vacinfo[i].date))
+        {
+            cout << "\t输入无效，不符合日期格式。请重新输入！" << endl;
+        }
+    } while (!JudgeDate(vacinfolist.vacinfo[i].date));
+    do
+    {
+        cout << "\t\t手机：";
+        cin >> vacinfolist.vacinfo[i].phone;
+        if (!JudgePhone(vacinfolist.vacinfo[i].phone))
+        {
+            cout << "\t输入无效，手机号码只能由11位数字组成。请重新输入！" << endl;
+        }
+    } while (!JudgePhone(vacinfolist.vacinfo[i].phone));
+    int vac = 0;
+    do
+    {
+        cout << "\t\t选择疫苗：" << endl;
+        cout << "\t\t\t1.国药中生北京" << endl;
+        cout << "\t\t\t2.国药中生武汉" << endl;
+        cout << "\t\t\t3.北京科兴中维" << endl;
+        cout << "\t\t请选择（1-3）：";
+        cin >> vac;
+        switch (vac)
+        {
+        case 1:
+        {
+            vacinfolist.vacinfo[i].vac = "国药中生北京";
+            cout << "\t……国药中生北京疫苗接种信息录入成功……" << endl;
+        }
+        break;
+        case 2:
+        {
+            vacinfolist.vacinfo[i].vac = "国药中生武汉";
+            cout << "\t……国药中生武汉疫苗接种信息录入成功……" << endl;
+        }
+        break;
+        case 3:
+        {
+            vacinfolist.vacinfo[i].vac = "北京科兴中维";
+            cout << "\t……北京科兴中维疫苗接种信息录入成功……" << endl;
+        }
+        break;
+        default:
+            cout << "输入有误，请重新输入" << endl;
+            break;
+        }
+    } while (vac < 1 || vac > 3);
+    vacinfolist.V_size++;
+    ofstream ofs;
+    ofs.open("Record2.txt", ios::out | ios::app);
+    ofs << vacinfolist.vacinfo[i].name << "\t"
+        << vacinfolist.vacinfo[i].sex << "\t"
+        << vacinfolist.vacinfo[i].age << "\t"
+        << vacinfolist.vacinfo[i].date << "\t"
+        << vacinfolist.vacinfo[i].phone << "\t"
+        << vacinfolist.vacinfo[i].vac << "\n";
+    // cout << "\t\t……………患者信息录入成功……………" << endl;
+    // cout << "\t\t是否继续录用[Y/N]：";
+    // cin >> flag;
+    // if (flag == 'Y' || flag == 'y')
+    // {
+    // ofs << "end" << endl;
+    ofs.close();
+    //     }
+    // } while (flag == 'Y' || flag == 'y');
+    cout << "\t\t感谢使用！";
+    system("pause");
+    system("cls");
+}
 int main()
 {
     char meun;
@@ -407,16 +585,17 @@ int main()
         case '2':
         {
             SortPatient();
+            break;
         }
-        break;
         case '3':
         {
-            // SearchPatient();
+            SearchPatient();
             break;
         }
         case '4':
         {
-            // AddVacInfo();
+            AddVacInfo();
+            break;
         }
         break;
         case '5':
